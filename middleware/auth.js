332def915eec3c1178f49272b52f5bd506b79823
Khaled Soliman <khaledsoli111@aucegypt.edu>
2020-09-25 00:57:20 +0200

    updated auth middleware response

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,18 @@
 const admin = require('../services/firebase');
+const logger = require('../log/logger')('Backend');
 
-const authMiddleware = (whiteList =[]) => (req, res, next) => {
+const authMiddleware = (whiteList = []) => (req, res, next) => {
     //skip whitelisted urls
     // if(whiteList.find(req.baseUrl))
     //     next();
     //authenticate request
     return admin.auth().verifyIdToken(req.headers.authorization)
-        .then(function(decodedToken) {
+        .then(function (decodedToken) {
             req.uid = decodedToken.uid;
             return next();
-        }).catch(function(error) {
-            return res.send(401, 'Failed to authenticate your request');
+        }).catch(function (error) {
+            logger.info(error);
+            return res.status(401).send('Failed to authenticate your request');
         });
 };
 
